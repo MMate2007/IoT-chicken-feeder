809fdaa9f487b4317a7e9c718768721fd7009aba
MMate2007 <72655257+MMate2007@users.noreply.github.com>
2020-10-10 19:25:49 +0200

    Forráskód kommentálása
    Megjegyzéseket adtam a forráskódhoz, hogy könnyebben meg lehessen érteni a kódot.

--- Tel.ino
+++ Tel.ino
@@ -1,4 +1,5 @@
 #define BLYNK_PRINT Serial
+//Könyvtárak inicializálása
 #include <Blynk.h>
 #include <SPI.h>
 #include <BlynkSimpleEthernet.h>
@@ -6,41 +7,42 @@
 #include <TimeLib.h>
 #include <WidgetRTC.h>
 
-char auth[] = "";
+char auth[] = "XXXXXXXXXXXX"; //Ide írja be a Blynk által küldött auth-token-t!
 
 #define W5100_CS  10
 #define SDCARD_CS 4
 
-WidgetTerminal terminal(V5);
+WidgetTerminal terminal(V5); //Terminál inicializálása
 BlynkTimer timer;
 WidgetRTC rtc;
 
-Servo csap;
-
+Servo csap; //Szervo motor inicializálása
+//Változók inicializálása
 String ido;
 int last_position;
 bool ir = false;
 bool af = false;
+//A következő metódusok segítségével megadhatjuk, hogy mi történjen ha módosul a zárójelben levő PIN értéke.
 BLYNK_WRITE(V2)
 {
-  if (param.asInt() == 0)
+  if (param.asInt() == 0)//Ha a V2-es PIN értéke 0 akkor...
   {
-    csap.write(-45);
-    terminal.println("Zárva.");
+    csap.write(-45);//Servo motor mozgatása
+    terminal.println("Zárva.");//Írás a terminálra.
   }
   if (param.asInt() == 1)
   {
     csap.write(45);
     terminal.println("Nyitva.");
   }
-  terminal.flush();
+  terminal.flush();//Kiírás a terminálra.
 }
 
 BLYNK_WRITE(V1)
 {
   if (param.asInt() == 1)
   {
-   etetes(1);
+   etetes(1);//Etetés. Ez egy saját metódus melynek definíciója alul található.
    terminal.println("Etetés...");
   }
 }
@@ -63,11 +65,11 @@ BLYNK_WRITE(V5)
 {
   if (String("Servo ki") == param.asStr())
   {
-    servo_off();
+    servo_off();//Saját metódus. Leírás alul.
   }
   if (String("Servo be") == param.asStr())
   {
-    servo_on();
+    servo_on();//Saját metódus. Leírás alul.
   }
   terminal.flush();
 }
@@ -77,8 +79,8 @@ BLYNK_CONNECTED() {
 }
 
 
-void etetes(int merteke){
-  if (csap.attached() == false)
+void etetes(int merteke){ //Saját metódus definiálása az etetésre
+  if (csap.attached() == false) //Ha a servo motor nincs csatlakoztatva akkor csatlakoztatjuk
   {
     servo_on();
     ido = "ki";
@@ -103,41 +105,43 @@ void etetes(int merteke){
      csap.write(-45);
      break;
     }
-    if (ido == "ki")
+    if (ido == "ki")//Ha ki volt kapcsolva az etetés előtt a motor akkor etetés után is kikapcsoljuk.
     {
       servo_off();
       orakiiras();
     }
 }
 
-void orakiiras() {
+void orakiiras() {//Saját metódus mely kiírja egy változóba az órát és az időt.
   ido = String(hour()) + ":" + minute();
 }
-void servo_on() {
-  csap.write(last_position);
-  csap.attach(3);
+void servo_on() {//Szervo motor csatlakoztatása
+  csap.write(last_position);//Szervo motor visszaállítása a kikapcsolás előtti pozícióba
+  csap.attach(3);//Csatlakoztatás
   terminal.println("Motor be!");
   delay(10000);
 }
-void servo_off() {
+void servo_off() {//Szervo motor lekapcsolása
   delay(10000);
   last_position = csap.read();
-  csap.detach();
+  csap.detach();//Lekapcsolás
   terminal.println("Motor ki!");
 }
-
+//A setup() metódus csak egyszer az Arduino indítása után fut le.
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600);//Soros monitor inicializálása
   pinMode(SDCARD_CS, OUTPUT);
-  pinMode(3, OUTPUT);
+  pinMode(3, OUTPUT);//Motor PIN-je a 3-mas ezen kapja az inpulzusokat, úgyhogy ez kimenet.
   digitalWrite(SDCARD_CS, HIGH);
   csap.attach(3);
   Blynk.begin(auth);
   setSyncInterval(30*60);
   timer.setInterval(10000L, orakiiras);
+  //Servo motor mozgatása egy kicsit.
   etetes(1);
   csap.write(-45);
   orakiiras();
+  //Applikáció szinkronizálása
   Blynk.virtualWrite(V2, 0);
   Blynk.virtualWrite(V0, 0);
 }
@@ -146,7 +150,8 @@ void loop()
 {
   Blynk.run();
   timer.run();
-  orakiiras();
+  orakiiras(); //Idő lekérése
+  //Ha az autómata etetés be van kapcsolva akkor etetünk autómatán.
   if (af == true)
   {
     if (ido == "9:0" || ido == "11:0" || ido == "13:30")
@@ -173,11 +178,13 @@ void loop()
       ir = false;
     }
   }
+  //Szervo motor kikapcsolása este.
   if (ido == "16:5")
   {
     servo_off();
     delay(60000);
   }
+  //Szervo motor bekapcsolása reggel.
   if (ido == "7:55")
   {
     servo_on();
